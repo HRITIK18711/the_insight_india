53bc2cf4031af31f51b9162344e2099b4bf84b0f
HRITIK18711 <hritikupadhyay2002@gmail.com>
2025-08-20 16:10:35 +0530

    Code Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 // news_portal App.jsx
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Header from './Component/Header';
-import MenuBar from './Component/MenuBar';
-import Advertisement from './Component/Advertisment';
-import Main_News_Section from './Component/Main_News_Section';
-import Home_Trending_News from './Component/Home_Trending_News';
-import Home_Sports_News from './Component/Home_Sports_News';
-import Latest_News from './Component/Latest_News';
-import Home_Entertainment_News from './Component/Home_Entertainment_News';
-import Footer from './Component/Footer';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import Header from "./Component/Header";
+import MenuBar from "./Component/MenuBar";
+import Advertisement from "./Component/Advertisment";
+import Main_News_Section from "./Component/Main_News_Section";
+import Home_Trending_News from "./Component/Home_Trending_News";
+import Home_Sports_News from "./Component/Home_Sports_News";
+import Latest_News from "./Component/Latest_News";
+import Home_Entertainment_News from "./Component/Home_Entertainment_News";
+import Footer from "./Component/Footer";
 import InsightVideo from "./Component/InsightVideo";
 import Politics from "./Component/Politics";
 import National from "./Component/National";
@@ -26,28 +32,25 @@ import Film_review from "./Component/Film_review";
 import Audio from "./Component/Audio";
 import Jobs from "./Component/Jobs";
 import Current_Affair from "./Component/Current_Affair";
-import Mass_comm from './Component/Mass_comm';
+import Mass_comm from "./Component/Mass_comm";
 import Miscal from "./Component/Miscal";
 
+// ✅ Main content handles home vs other pages
 function MainContent() {
   const location = useLocation();
-  const isHome = location.pathname === '/';
+  const isHome = location.pathname === "/";
 
   return (
-    <>
+    <main className="flex flex-col gap-6 px-2 sm:px-4 md:px-6 lg:px-12">
       {isHome ? (
-        // Homepage Layout
         <>
-          {/* <MenuBar /> */}
           <Advertisement />
           <Main_News_Section />
           <Home_Trending_News />
           <Home_Sports_News />
           <Home_Entertainment_News />
-          
         </>
       ) : (
-        // Other Routes
         <Routes>
           <Route path="/latest-news" element={<Latest_News />} />
           <Route path="/insight" element={<InsightVideo />} />
@@ -70,17 +73,30 @@ function MainContent() {
           <Route path="/miscal" element={<Miscal />} />
         </Routes>
       )}
-    </>
+    </main>
   );
 }
 
 function App() {
   return (
     <Router>
-      <Header />
-      <MenuBar />
-      <MainContent />
-      <Footer/>
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        {/* Header */}
+        <Header />
+
+        {/* Sticky MenuBar */}
+        <div className="sticky top-0 z-50 bg-white shadow-md">
+          <MenuBar />
+        </div>
+
+        {/* Main Responsive Content */}
+        <div className="flex-grow">
+          <MainContent />
+        </div>
+
+        {/* Footer */}
+        <Footer />
+      </div>
     </Router>
   );
 }
